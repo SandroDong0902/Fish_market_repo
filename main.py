a7fb9080901ac51ebe22a7189928959ce7afcba6
Sandro Dong <sandro@sandros-air.home>
2022-01-25 15:35:08 +0000

    Added upload to mongoDB

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import boto3
 import pandas as pd
+from pprint import pprint as pp
+import pymongo
 
 
 class FishMarket:
@@ -10,6 +12,9 @@ class FishMarket:
         self.s3_resource = boto3.resource('s3')
         self.bucket_name = 'data-eng-resources'
 
+        self.client = pymongo.MongoClient('mongodb://3.127.136.241:27017/Sparta')
+        self.db = self.client['fish_market']
+
         self.bucket_contents = self.s3_client.list_objects_v2(Bucket=self.bucket_name)
 
         self.processing_files = []
@@ -17,8 +22,11 @@ class FishMarket:
         files = self.read_from_s3()
         df = self.get_df(files)
         df = self.calculate_avg(df)
-        self.out_csv(df)
-        self.upload()
+        csv = self.out_csv(df)
+        d = self.df_to_dict(df)
+        self.upload_to_mongo(d)
+        self.download_check()
+        #self.upload()
 
     def read_from_s3(self):
 
@@ -44,12 +52,30 @@ class FishMarket:
         return df.groupby('Species').mean()
 
     def out_csv(self, df):
+
         return df.to_csv("Sandro-fish-market.csv")
 
     def upload(self):
 
         self.s3_client.upload_file(Filename = 'Sandro-fish-market.csv', Bucket = self.bucket_name, Key = 'Data26/fish/Sandro-fish-market.csv')
         print('Document successfully uploaded')
+
+    def df_to_dict(self, df):
+        df['Species'] = df.index
+        return df.to_dict(orient = 'records')
+
+    def upload_to_mongo(self, d):
+
+        self.db.fish_market.drop()
+        self.db.fish_market.insert_many(d)
+        print('Successfully uploaded to mongoDB.', '\n')
+
+    def download_check(self):
+
+        data = self.db.fish_market.find()
+        for d in data:
+            pp(d)
+        print('\n', 'It works!') # I don't mean it works correctly 100%
 
 
 f = FishMarket()
